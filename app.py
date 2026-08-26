9695ea19681a83669d6b3f55626a4f8d7716c545
Saad <m.saadnaeem203@gmail.com>
2026-08-27 04:41:45 +0500

    Improve HTTP request handling

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import requests
-response = requests.get("https://example.com")
-print("Status code:", response.status_code)
+url = "https://example.com"
+try:
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    
+    print("Request successful!")
+    print("Status code:", response.status_code)
+    print("Website:", response.url)
+
+except requests.exceptions.RequestException as e:
+    print("Request failed:", e)
+
