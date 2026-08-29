9eafe30ced6b7361acb3fcb5976f20a768f03769
Saad <m.saadnaeem203@gmail.com>
2026-08-30 03:39:54 +0500

    Add repeated website monitoring

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import requests
+import time
 
 url = "https://example.com"
 
-try:
-   response = requests.get(url, timeout=10)
-   response.raise_for_status()
+for check in range(1, 6):
+    print("Check", check)
 
-   if response.status_code == 200:
-      print("Status: UP")
-      print("Status code:", response.status_code)
-      print("Website:", response.url)
-   else:
-      print("Status: DOWN")
-      print("Status code:", response.status_code)
+    try:
+        response = requests.get(url, timeout=10)
 
-except requests.exeptions.RequestException as e:
-     print("Status: DOWN")
-     print("Request failed:", e)
+        if response.status_code == 200:
+            print("Status: UP")
+            print("Status code:", response.status_code)
+        else:
+            print("Status: DOWN")
+            print("Status code:", response.status_code)
+
+    except requests.exeptions.RequestException as e:
+         print("Status: DOWN")
+         print("Request failed:", e)
+
+    print("-------------------")
+    time.sleep(5)
